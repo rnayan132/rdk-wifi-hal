bfe508f5356bd75dc850b2e8f9e59c279622ae6b
Kondamma12345 <kkudum797@apac.comcast.com>
2026-01-06 09:44:07 +0000

    RDKB-62695: Fix High priority coverity issue.
    Reason for change: To fix the Coverity issues which have high  impact.
    Test Procedure: Build should be successful and the regression test should also succeed.
    
    Risks: Low
    Priority: P1
    Signed-off-by: Kondamma_Kudumula@comcast.com

--- platform/broadcom/platform.c
+++ platform/broadcom/platform.c
@@ -3086,7 +3086,10 @@ static int get_sta_list_handler(struct nl_msg *msg, void *arg)
     }
 
     sta_list->macs = calloc(sta_list->num, sizeof(mac_address_t));
-
+    if (sta_list->macs == NULL) {
+        wifi_hal_stats_error_print("%s:%d Memory allocation failed\n", __func__, __LINE__);
+        goto error;
+    }
     if (tb_vendor[RDK_VENDOR_ATTR_STA_LIST] == NULL) {
         wifi_hal_stats_error_print("%s:%d STA list data is missing\n", __func__, __LINE__);
         goto error;
@@ -3117,8 +3120,10 @@ static int get_sta_list_handler(struct nl_msg *msg, void *arg)
     return NL_SKIP;
 
 error:
-    free(sta_list->macs);
-    sta_list->macs = NULL;
+    if (sta_list->macs != NULL) {
+        free(sta_list->macs);
+        sta_list->macs = NULL;
+    }
     sta_list->num = 0;
     return NL_SKIP;
 }
