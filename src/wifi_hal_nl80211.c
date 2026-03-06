95291d455adfed70a752f3241b8c478050183fd8
rnayan132 <Raghava_Nayani@comcast.com>
2026-03-06 12:13:35 +0530

    Rdkb 62668 medium coverity issues (#589)
    RDKB-62668: Fix medium priority Coverity issue.
    
    Reason for change: To fix the Coverity issues which have medium impact.
    Test Procedure: Build should be successful and the regression test should also succeed.
    
    Risks: Low
    Priority: P1
    
    Signed-off-by: Raghava_Nayani@comcast.com
    
    * RDKB-62668: Fix medium priority Coverity issue.
    
    Reason for change: To fix the Coverity issues which have medium impact.
    Test Procedure: Build should be successful and the regression test should also succeed.
    
    Risks: Low
    Priority: P1
    
    Signed-off-by: Raghava_Nayani@comcast.com
    
    ---------
    
    Signed-off-by: Raghava_Nayani@comcast.com
    Co-authored-by: Sathish Kumar Gnanasekaran <gsathish86@gmail.com>
    Co-authored-by: Raja Shah <167130396+rshah-1@users.noreply.github.com>

--- src/wifi_hal_nl80211.c
+++ src/wifi_hal_nl80211.c
@@ -12694,8 +12694,11 @@ wifi_drv_get_hw_feature_data(void *priv, u16 *num_modes, u16 *flags, u8 *dfs_dom
     *dfs_domain = 0;
 #if !(defined(VNTXER5_PORT) || defined(TARGET_GEMINI7_2))
     msg = nl80211_drv_cmd_msg(g_wifi_hal.nl80211_id, NULL, NLM_F_DUMP, NL80211_CMD_GET_WIPHY);
-    nla_put_flag(msg, NL80211_ATTR_SPLIT_WIPHY_DUMP);
     if (msg == NULL) {
+        wifi_hal_error_print("%s:%d: Failed to create message\n", __func__, __LINE__);
+        return NULL;
+    }
+    nla_put_flag(msg, NL80211_ATTR_SPLIT_WIPHY_DUMP);
 #else
     u32 feat;
     feat = get_nl80211_protocol_features(g_wifi_hal.nl80211_id);
@@ -12704,11 +12707,15 @@ wifi_drv_get_hw_feature_data(void *priv, u16 *num_modes, u16 *flags, u8 *dfs_dom
         *flags = NLM_F_DUMP;
 
     msg = nl80211_drv_cmd_msg(g_wifi_hal.nl80211_id, NULL, *flags, NL80211_CMD_GET_WIPHY);
-    if (!msg || nla_put_flag(msg, NL80211_ATTR_SPLIT_WIPHY_DUMP)) {
-#endif
+    if (msg == NULL) {
+        wifi_hal_error_print("%s:%d: Failed to create message\n", __func__, __LINE__);
+        return NULL;
+    }
+    if (nla_put_flag(msg, NL80211_ATTR_SPLIT_WIPHY_DUMP)) {
         nlmsg_free(msg);
         return NULL;
     }
+#endif
     
     if (nl80211_send_and_recv(msg, phy_info_get_hw_feature_handler, &result, NULL, NULL) == 0) {
         struct hostapd_hw_modes *modes;
